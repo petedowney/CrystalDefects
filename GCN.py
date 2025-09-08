7b2697ba35f2efec54ac5b949a8c9ae78a713832
petedowney <peter.d.downey@gmail.com>
2025-09-07 23:05:31 -0400

    further work

--- GCN.py
+++ GCN.py
@@ -1,12 +1,13 @@
 
 from torch.autograd import Variable
 import torch
+import torch.nn as nn
 
 import os
 from torchvision.io import read_image
 from torch.utils.data import Dataset
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv, global_mean_pool, global_max_pool, GlobalAttention, GATConv
 from torch_geometric.utils import dense_to_sparse
 from torch_geometric.data import Data
 from tqdm import tqdm
@@ -17,67 +18,92 @@ from tqdm import tqdm
 class GraphConv(torch.nn.Module):
     def __init__(self, input_size, lr):
         super(GraphConv, self).__init__()
+
+        ATOM_AMOUNT = 118
+        self.embedding = nn.Embedding(ATOM_AMOUNT, input_size)
+
+        self.GCN = GATConv(input_size, input_size)
+
+        # After pooling, the input size is 500 (output of last GCN layer)
+        self.linear = torch.nn.Linear(input_size, 10)
+        self.linear2 = torch.nn.Linear(10, 1)
+
+        self.test = torch.nn.Linear(1, 1)
+
+        self.loss = nn.MSELoss()
+        self.optimizer = torch.optim.SGD(self.parameters(), lr=lr, weight_decay=0)
+
+        gate_nn = nn.Sequential(
+            nn.Linear(input_size, 1),
+            nn.Sigmoid()  # attention scores in [0,1]
+        )
+        self.att_pool = GlobalAttention(gate_nn)
+
+        self.lossHistory = []
+        self.valLossHistory = []
+
+    def forward(self, inputFeatures, edge_index, edge_weight, batch):
         
-        self.GCN1 = GCNConv(input_size, 100)
-        self.GCN2 = GCNConv(100, 50)
-        self.GCN3 = GCNConv(50, 25)
+        #print('a', inputFeatures[0])
+        x = self.GCN(inputFeatures, edge_index)
+        #x = F.leaky_relu(x)
 
+        #print('b', x[0])
+        
+        for n in range(2):
+            x = self.GCN(x, edge_index)
+           # x = F.leaky_relu(x)
 
-        self.linear = torch.nn.Linear(25, 25)
-        self.linear2 = torch.nn.Linear(25, 10)
-        self.linear3 = torch.nn.Linear(10, 1)
-    
+        #print('c', x[0])
 
+        x = global_mean_pool(x, batch)
 
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=lr, weight_decay=.001)
-        self.loss = torch.nn.MSELoss()
+        #print('d', x[0])
 
-    def forward(self, inputFeatures, edge_index):
-        x = self.GCN1(inputFeatures, edge_index)
-        x = F.relu(x)
-        x = self.GCN2(x, edge_index)
-        x = F.relu(x)
-        x = self.GCN3(x, edge_index)
-        x = F.relu(x)
         x = self.linear(x)
-        x = F.relu(x)
+        x = F.leaky_relu(x)
         x = self.linear2(x)
-        x = F.relu(x)
-        x = self.linear3(x)
-        x = F.relu(x)
-
         return x
 
     def trainModel(self, data_train, data_val, epochs, verbose=True):
-        
+        torch.manual_seed(10)
         for epoch in range(epochs):
             epoch_loss = 0
             self.train()  # set to train mode
-            with tqdm(enumerate(data_train), total=len(data_train), desc=f"Epoch {epoch+1}/{epochs}") as pbar:
+            with tqdm(enumerate(data_train), total=len(data_train), desc=f"Epoch {epoch+1}/{epochs}", disable=not verbose) as pbar:
+                
                 for i, data in pbar:
                     inputFeatures = data.x
                     edge_index = data.edge_index
+                    edge_weight = data.edge_weight
                     outputFeature = data.y
-                    output = self.forward(inputFeatures, edge_index)
+                    batch = data.batch if hasattr(data, 'batch') else None
+                    output = self.forward(inputFeatures, edge_index, edge_weight, batch)
+                    #print(output, outputFeature)
                     loss = self.loss(output, outputFeature)
+                    self.optimizer.zero_grad()
                     loss.backward()
                     self.optimizer.step()
-                    self.optimizer.zero_grad()
                     epoch_loss += loss.item()
                     pbar.set_postfix({'loss': loss.item()})
+                epoch_loss /= len(data_train)
+                self.lossHistory.append(epoch_loss)
             val_loss = 0
             self.eval()  # set to eval mode
             with torch.no_grad():
                 for data in data_val:
                     inputFeatures = data.x
                     edge_index = data.edge_index
+                    edge_weight = data.edge_weight
                     outputFeature = data.y
-                    output = self.forward(inputFeatures, edge_index)
+                    batch = data.batch if hasattr(data, 'batch') else None
+                    output = self.forward(inputFeatures, edge_index, edge_weight, batch)
                     loss = self.loss(output, outputFeature)
                     val_loss += loss.item()
             val_loss /= len(data_val)
+            self.valLossHistory.append(val_loss)
             if verbose:
-                print(f'Epoch {epoch+1} Loss --> {epoch_loss/len(data_train):.4f} | Val Loss --> {val_loss:.4f}')
+                print(f'Epoch {epoch+1} Loss --> {epoch_loss:.4f} | Val Loss --> {val_loss:.4f}')
 
 
 class InterpreterDataset(Dataset):
@@ -95,11 +121,11 @@ class InterpreterDataset(Dataset):
         return len(self.inputFeaturesList)
 
     def __getitem__(self, idx):
-
-        # Convert dense adjacency matrix to edge index
-        edge_index, _ = dense_to_sparse(self.adjacencyMatrixList[idx])
-        edge_index = edge_index[:, :1000]  # Limit to 1000 edges
-
-        data = Data(x=self.inputFeaturesList[idx], edge_index=edge_index, y=self.outputFeatureList[idx])
-
+        # Convert dense adjacency matrix to edge index and edge_weight (distance)
+        edge_index, edge_weight = dense_to_sparse(self.adjacencyMatrixList[idx])
+        # Optionally limit to 1000 edges
+        # edge_index = edge_index[:, :50]
+        # edge_weight = edge_weight[:50]                    self.optimizer.zero_grad()
+        data = Data(x=self.inputFeaturesList[idx], edge_index=edge_index, edge_weight=edge_weight.t().contiguous(), y=self.outputFeatureList[idx])
+        #print(self.inputFeaturesList[idx, :, 0], self.inputFeaturesList[idx, 0, -1],  self.outputFeatureList[idx])
         return data
